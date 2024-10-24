d7c41d30a779c484bf7a5eb55bf102c0a03def88
Hristo Genev <harrygg@gmail.com>
2024-10-24 14:29:41 +0300

    Scheduled daily update

--- run.bat
+++ run.bat
@@ -4,6 +4,18 @@ set APPDIR=%~dp0
 cd %APPDIR%
 %APPDIR%\bin\app.exe grab -f %APPDIR%\config -o %APPDIR% -l 0
 
+cp report.html ..\harrygg.github.io\EPG\report.html
+cp report.js ..\harrygg.github.io\EPG\report.js
+
+git pull
+git status
+git add -A
+git commit -m "Scheduled daily update"
+git push
+git status
+
+cd %APPDIR%..\harrygg.github.io
+
 git pull
 git status
 git add -A
